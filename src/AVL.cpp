d2f2f0af68a49ab6c8b0b80c39db44a8fd3c86dd
IasoInc <iasox.inc@gmail.com>
2025-09-28 01:46:13 -0400

    9.26.2025

--- src/AVL.cpp
+++ src/AVL.cpp
@@ -6,11 +6,88 @@
 #include <iostream>
 using namespace std;
 
-bool AVL::insert(std::string name, int UFID) {
-    data.emplace_back(name, UFID);
-    return false;
+//referenced from the lecture slides
+Node* AVL::insertHelper(Node* currentNode, string name, int id) {
+
+    if (id == currentNode->UFID) {
+        cout << "Unsuccessful" << endl;
+        return currentNode;
+    }
+
+    //addition state
+    if (id > currentNode->UFID) {
+        //if null add the node to the right
+        if (currentNode->right == nullptr) {
+            Node* newNode = new Node(name, id);
+            currentNode->right = newNode;
+            cout << "Successful" << endl;
+            //recalculate the node height
+        }
+        //keep going down the tree
+        else {
+            currentNode->right = insertHelper(currentNode->right, name, id);
+        }
+    }
+    else if (id < currentNode->UFID) {
+        //if null add the node to the left
+        if (currentNode->left == nullptr) {
+            Node* newNode = new Node(name, id);
+            currentNode->left = newNode;
+            cout << "Successful" << endl;
+        }
+        //keep going down the list
+        else {
+            currentNode -> left = insertHelper(currentNode->left, name, id);
+        }
+    }
+
+
+    //update height of the nodes by going through the list below - max gotten from https://cplusplus.com/reference/algorithm/max/
+    currentNode->height = 1 + max(calculateHeight(currentNode->right), calculateHeight(currentNode->left));
+
+    //balance the tree - credits Slide 74 Lecture Slides 4
+    int heightDifference = calculateHeight(currentNode->left) - calculateHeight(currentNode->right);
+
+    //left left or left-right
+    if (heightDifference >= 2) {
+        int heightDifferenceChild = calculateHeight(currentNode->left->left) - calculateHeight(currentNode->left->right);
+        if (heightDifferenceChild >= 1) {
+            leftLeft(currentNode);
+        }
+        else {
+            leftRight(currentNode);
+        }
+    }
+    //right right or right-left
+    if (heightDifference <= -2) {
+        int heightDifferenceChild = calculateHeight(currentNode->right->right) - calculateHeight(currentNode->right->left);
+        if (heightDifferenceChild >= 1) {
+            rightRight(currentNode);
+        }
+        else {
+            rightLeft(currentNode);
+        }
+    }
+
+    return currentNode;
 }
 
+int AVL::calculateHeight(Node* node) {
+    if (node == nullptr) {
+        return 0;
+    }
+    return node->height;
+}
+
+void AVL::insert(std::string name, int UFID) {
+    if (root == nullptr) {
+        Node* newNode = new Node(name, UFID);
+        root = newNode;
+        return;
+    }
+    root = insertHelper(root, name, UFID); //equal to root, because of shifting
+}
+/*
 bool AVL::erase(int) {
     return true;
 }
@@ -30,3 +107,39 @@ void AVL::remove(int ID) {
         }
     }
 }
+
+*/
+//credits lecture slide 12 (4)
+void AVL::rightRight(Node* node) {
+    Node* grandChild = node->right->left;
+    Node* parent = node->right;
+    parent->left = node;
+    node->right = grandChild;
+
+    node->height  = 1 + max(calculateHeight(node->left),  calculateHeight(node->right));
+    parent->height = 1 + max(calculateHeight(parent->left), calculateHeight(parent->right));
+}
+
+void AVL::leftLeft(Node *node) {
+    Node* grandchild = node->left->right;
+    Node* parent = node->left;
+    parent->left = node;
+    node->left = grandchild;
+
+    node->height  = 1 + max(calculateHeight(node->left),  calculateHeight(node->right));
+    parent->height = 1 + max(calculateHeight(parent->left), calculateHeight(parent->right));
+}
+
+void AVL::rightLeft(Node *node) {
+    leftLeft(node->right);
+
+
+}
+
+void AVL::leftRight(Node *node) {
+}
+
+AVL::~AVL() {
+}
+
+
--- src/AVL.h
+++ src/AVL.h
@@ -8,15 +8,50 @@
 #include<iostream>
 #include<vector>
 
+using namespace std;
+
+struct Node {
+
+    int UFID;
+    string name;
+    int height;
+    Node* left;
+    Node* right;
+
+    Node(string name, int UFID) {
+        this->name = name;
+        this->UFID = UFID;
+        left = nullptr;
+        right = nullptr;
+        height = 1;
+    }
+};
+
 
 class AVL {
+private:
+    Node* root = nullptr;
+    Node* insertHelper(Node* currentNode, string name, int id); //want to keep private so cannot access on main.
+    static int calculateHeight(Node* node);
+
+
 public:
-    std::vector<std::pair<std::string, int>> data; //store all the UF student names, and ID's here.
-    bool insert(std::string name, int UFID);
-    bool erase(int);
-    std::vector<std::pair<std::string, int>> inorder();
-    std::vector<std::pair<std::string, int>> preorder();
-    void remove(int ID);
+    //Node* returnRoot();
+    void insert(std::string name, int UFID);
+    //bool erase(int);
+    //std::vector<std::pair<std::string, int>> inorder();
+    //std::vector<std::pair<std::string, int>> preorder();
+    //void remove(int ID);
+
+
+    //rotations:
+    void rightRight(Node* node);
+    void leftLeft(Node* node);
+    void rightLeft(Node* node);
+    void leftRight(Node* node);
+
+    //destructor
+    ~AVL(); //use a post order traversal
 };
 
 
